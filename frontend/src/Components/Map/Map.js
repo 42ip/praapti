28b9cb2214120eb2fc9a93b0dcae15d204690f57
tirth0 <tirtharajsengupta@gmail.com>
2021-04-03 03:34:56 +0530

    mapbox draw

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,7 +1,11 @@
-import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import React, { useState } from 'react';
+import MapGL,{NavigationControl} from '@urbica/react-map-gl';
+import Draw from '@urbica/react-map-gl-draw';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 
-export default function App() {
+
+export default function Map() {
     const [viewport, setViewport] = useState({
       latitude: 45.4211,
       longitude: -75.6903,
@@ -9,32 +13,37 @@ export default function App() {
       height: "85vh",
       zoom: 10
     });
-    const [selectedPark, setSelectedPark] = useState(null);
-  
-    useEffect(() => {
-    //   const listener = e => {
-    //     if (e.key === "Escape") {
-    //       setSelectedPark(null);
-    //     }
-    //   };
-    //   window.addEventListener("keydown", listener);
-  
-    //   return () => {
-    //     window.removeEventListener("keydown", listener);
-    //   };
-    }, []);
+    const [data, setData] = useState({
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              coordinates: [-122.41411987304815, 37.792209769935084],
+              type: 'Point'
+            }
+          }
+        ]
+      });
+      const navControlStyle= {
+        right: 10,
+        top: 10
+      };
   
     return (
-      <div>
-        <ReactMapGL
-          {...viewport}
-          mapboxApiAccessToken={`pk.eyJ1IjoidGlydGgwIiwiYSI6ImNrbjB0NHY1aTA5bTkycG56NXZhdXl4MGkifQ.1ywfUyHehaszWb-Q0tlb3g`}
-          mapStyle="mapbox://styles/mapbox/satellite-v9"
-          onViewportChange={viewport => {
-            setViewport(viewport);
-          }}
-        > 
-        </ReactMapGL>
-      </div>
+        <>
+        <MapGL
+           {...viewport}
+          style={{ width: '100%', height: '85vh' }}
+          mapStyle='mapbox://styles/mapbox/satellite-v9'
+          accessToken={"pk.eyJ1IjoidGlydGgwIiwiYSI6ImNrbjB0NHY1aTA5bTkycG56NXZhdXl4MGkifQ.1ywfUyHehaszWb-Q0tlb3g"}
+          onViewportChange={setViewport}
+        >
+          
+          <Draw data={data} onChange={(data) => setData(data)} />
+          
+        </MapGL>
+      </>
     );
   }
