b622f7d51b2a7f05907390d9b9e2a37b561ebd16
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2021-04-02 12:32:51 +0530

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>frontend</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
