4175a267cdc85826a8229f8f11a5b2e924413cc2
Raghavendra Achar C <61545949+Raghav14200@users.noreply.github.com>
2021-04-03 14:18:57 +0530

    Responsive (#8)
    * footer_fix
    
    * responsive_landing_page

--- frontend/src/Components/Privacy.js
+++ frontend/src/Components/Privacy.js
@@ -3,6 +3,9 @@ import './css/Privacy.css'
 export default class Privacy extends Component {
     render() {
         return (
+            <div>
+                <h1>Privacy Policy</h1>
+                <div className="footer_fix">
             <div className="privacy_container">
                 <h2>Yield Privacy Policy</h2>
                 <div className="para1">
@@ -12,7 +15,8 @@ export default class Privacy extends Component {
                 <div className="para2">
                     Yield collects, processes, and retains information about you when you visit the Websites or use the Services. You may choose to provide Yield with information, such as your name, email address, company information, street address, telephone number, or other information, to use the Services or so we can follow up with you after your visit. If you choose to participate in the Yield Community, you may choose to provide us with additional information such as social media profiles, education information, experience, and other information. Certain data elements requested of you by the Websites are “mandatory” or “required,” and failure to provide this data may make it impossible for Yield to provide its Services to you. In cases where requested data elements are not mandatory or required, you may choose not to provide the requested data without any consequences to the availability or the functioning of the Services. We may combine other publicly available information with the information that you provide to us to enhance and optimize our Websites and Services for you. Yield may observe your activities, interactions, preferences, and other computer and connection information (such as IP address and geographic location) relating to your use of the Websites and the Services. Yield may also use log files, cookies and similar technologies to collect information about the pages you view, links you click on, and other actions you may take when accessing the Websites and Services. For more information about our use of these technologies, please see the Cookies section below. We may collect and store this information and combine this information with other personal information you may have provided.
                 </div>
-                <div className="footer_fix"></div>
+                </div>
+                </div>
             </div>
         );
     }
