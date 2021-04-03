486d330ace1f83e15d89bad1f8df424a1d869552
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2021-04-03 16:12:18 +0530

    add dummy data in UserProfs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import Home  from './Components/Home';
 import Privacy from './Components/Privacy';
 import Map from './Components/Map/Map'
 import {Component} from 'react';
+import IMS from './Components/UserProfs/IMS';
 
 
 class App extends Component{
@@ -23,8 +24,11 @@ class App extends Component{
         <Navbar/>
       </header>
         <Switch>
-        <Route exact path="/Reg">
+            <Route exact path="/Reg">
               <Auth/>
+            </Route>
+            <Route exact path="/IMS">
+                <IMS/>
             </Route>
           <Route exact path="/" component={Home}/>
           <Route exact path="/privacy" component={Privacy} />
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import farmer from '../images/farmer.png';
 import { Link } from 'react-router-dom';
-
 export default class Home extends Component {
     render() {
         return (
@@ -9,16 +8,16 @@ export default class Home extends Component {
          <div className="farmer">
             <img src={farmer} alt="farmer with his donkey"/>    
         </div>
-        <div class="container">
-            <div class="LandLords">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</div>
-            <div class="workers">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</div>
+        <div className="container">
+            <div className="LandLords">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</div>
+            <div className="workers">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</div>
         </div>
-            <div class="button_container">
+            <div className="button_container">
              <Link to="/reg">
              <button type="button" className="log">
              Login / SignUp
              </button>
-             </Link>
+            </Link>      
         </div>
         </div>
         );
